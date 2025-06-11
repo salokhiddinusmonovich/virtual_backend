dbe6b80dfc5f3356912a439708bedd3b665dee71
Salokhiddin <usmonovsalokhiddin@gmail.com>
2025-06-11 22:04:34 +0500

    deleted drf_spectacular

--- apps/accounts/views/auth_views.py
+++ apps/accounts/views/auth_views.py
@@ -2,8 +2,12 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from drf_spectacular.utils import extend_schema
+from drf_spectacular.openapi import AutoSchema
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiResponse
 
 from apps.accounts.serializers import (
     auth_serializers
@@ -14,13 +18,6 @@ class LoginAPIView(APIView):
     permission_classes = (AllowAny,)
     serializer_class = auth_serializers.LoginSerializer
 
-    @extend_schema(
-        request=auth_serializers.LoginSerializer,
-        responses={200: auth_serializers.LoginSerializer},
-        tags=['Auth'],
-        description='Login user'
-    )
-
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         try:
@@ -30,3 +27,18 @@ class LoginAPIView(APIView):
             raise InvalidToken(e.args[0])
         except Exception as e:
             return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterAPIView(APIView):
+    permission_classes = (AllowAny, )
+    serializer_class = auth_serializers.RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request}, files=request.FILES)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
